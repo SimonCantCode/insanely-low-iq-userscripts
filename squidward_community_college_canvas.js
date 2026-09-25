4c8316c51e2eedb204046c68ec6f664793f03954
Simon <simondepomian@gmail.com>
2026-09-25 16:18:33 +0200

    .

---
+++ squidward_community_college_canvas.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name     Squidward community college canvas
+// @version  1
+// @grant    https://canvas.kth.se/* // change accordingly
+// ==/UserScript==
+
+document.documentElement.style.setProperty('--ic-brand-header-image', "url('https://raw.githubusercontent.com/SimonCantCode/insanely-low-iq-userscripts/refs/heads/main/media/squidward.png')");
